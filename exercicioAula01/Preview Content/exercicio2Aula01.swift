cabc439c721174f81a429d2e10681bdee658c942
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-23 20:19:28 -0300

    Update exercicio2Aula01.swift

--- exercicioAula01/Preview Content/exercicio2Aula01.swift
+++ exercicioAula01/Preview Content/exercicio2Aula01.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct exercicio2Aula01: View {
     var body: some View {
-        Image("Image")
-            .frame(width:50,height: 50)
+        HStack{
+            Spacer()
+            HStack{
+                Image("Image")
+                    .resizable() //Redimensiona a imagem
+                    .frame(width:150,height:150) // tamanho do circulo
+                    .clipShape(Circle()) // Coloca em formato de circulo
+            }
+            Spacer()
+            VStack{
+                Text("Hackatruck")
+                    .foregroundColor(.red) // Muda a cor do texto
+                Text("77 Universidades")
+                    .foregroundColor(.blue)
+                Text("5 regiões")
+                    .foregroundColor(.purple)
+            }
+            Spacer()
+        }   
     }
 }
 
-#Preview {
+#Preview {  // Mostra a visualização do APP
     exercicio2Aula01()
 }
