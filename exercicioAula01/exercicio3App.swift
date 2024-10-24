0e07ca09d31eff63a47e1ebff4646b59d42c9e9c
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-24 16:46:24 -0300

    Update exercicio3App.swift

--- exercicioAula01/exercicio3App.swift
+++ exercicioAula01/exercicio3App.swift
@@ -1,51 +1,71 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by Turma01-23 on 24/10/24.
+//
+
 import SwiftUI
 
-struct exercicio3App: View {
-  var body: some View{
-    HStack{ 
-      ZStack(alignment: bottomLeading){ // Foto de perfil
-        Image("Image")
-          .resizable()
-          .frame(width: 150, height: 150)
-          .clipShape(Circle())
-        Circle() // Circulo de adicionar story (falta o maizinho)
-          .fill(Color.blue)
-          .frame(width: 10.0, height: 10.0)
-          .border(Color.white)
-      }
-      VStack{ //quadradinhos + profile editor
-        HStack{ //quadradinhos
-          Spacer()
-          VStack{
-            Text("0")
-              .bold()
-            Text("Photos")
-          }
-          VStack{
-            Text("0")
-              .bold()
-            Text("Follows")
-          }
-          VStack{
-            Text("0")
-              .bold()
-            Text("Following")
-          }
-          Spacer()   
+struct ContentView: View {
+    var body: some View{
+        VStack{
+            HStack{
+                ZStack(){ // Foto de perfil
+                    Image("Fernanda")
+                        .resizable()
+                        .frame(width: 130, height: 130)
+                        .clipShape(Circle())
+                        .padding(2)
+                    ZStack(){
+                        Circle() // Circulo de adicionar story (falta o maizinho)
+                            .fill(Color.blue)
+                            .frame(width: 20.0, height: 20.0)
+                            .offset(x:40, y:50)
+                        Image(systemName:"plus")
+                            .foregroundColor(.white)
+                            .offset(x:40, y:50)
+                            .padding()
+                    }
+                }
+                VStack{ //quadradinhos + profile editor
+                    HStack{ //quadradinhos
+                        Spacer()
+                        VStack{
+                            Text("0")
+                                .bold()
+                            Text("Photos")
+                        }
+                        VStack{
+                            Text("2k")
+                                .bold()
+                            Text("Follows")
+                        }
+                        VStack{
+                            Text("100")
+                                .bold()
+                            Text("Following")
+                        }
+                        Spacer()
+                    }
+                    HStack{
+                        Spacer()
+                        Text("Profile editor")
+                            .bold()
+                            .border(Color.gray.opacity(0.25))
+                            .frame(width: 200, height: 30)
+                            .background(Color.gray.opacity(0.25))
+                            .cornerRadius(5.0)
+                        Spacer()
+                    }
+                }
+            } // Fim de perfil + seguidores + profile
+            VStack{
+                Text("Fernanda Torres\n").bold() + Text("Atriz, escritora, cronista e roteirista brasileira.") .font(.system(size: 15))
+            }
         }
-        HStack{
-          Spacer()
-          Text("Profile editor")
-            .bold()
-            .background(Color.gray.opacity(0.25))
-            .cornerRadius(14.0)
-            .padding(14)
-          Spacer()
-        }
-      }
     }
-  }
 }
-#Preview{
-  exercicio3App()
+#Preview {
+    ContentView()
 }
